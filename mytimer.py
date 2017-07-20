3ef02bc28e9b0321edc20f3c8f4c0ab1fb2ab0c9
start712 <start712@ProtonMail.com>
2017-07-20 15:11:19 +0800

    更新定时timer

--- mytimer.py
+++ mytimer.py
@@ -20,23 +20,39 @@ schedule = sched.scheduler(time.time, time.sleep)
 sys.path.append(sys.prefix + "\\Lib\\MyWheels")
 reload(sys)
 sys.setdefaultencoding('utf8')
-
+start_time = 0
+end_time = 0
 class mytimer(object):
+    def __init__(self):
+        pass
+        #self.start_time = 0
+        #self.end_time = 0
+        
     # 被周期性调度触发的函数
     def execute_command(self, cmd, inc):
-        '''''
-        终端上显示当前计算机的连接情况
-        '''
-        print u'报时：%s' % datetime.datetime.now()
+        start_time = datetime.datetime.now()
         os.system(cmd)
-        schedule.enter(inc, 0, self.execute_command, (cmd, inc))
+        time.sleep(3)
+        end_time = datetime.datetime.now()
+        delay = round((end_time-start_time).total_seconds())
+        print 
+        print u'mytimer => 开始时间：%s' % start_time
+        print u'mytimer => 耗时:%smin' %(delay/60)
+        schedule.enter(int(inc-delay), 0, self.execute_command, (cmd, inc))
+        print u'mytimer => 结束时间：%s' % end_time
 
-    def cmd_timer(self, cmd, inc=60):
-        # enter四个参数分别为：间隔事件、优先级（用于同时间到达的两个事件同时执行时定序）、被调用触发的函数，
+    def cmd_timer(self, cmd, time_str, inc=60):
+        # enter四个参数分别为：间隔时间、优先级（用于同时间到达的两个事件同时执行时定序）、被调用触发的函数，
         # 给该触发函数的参数（tuple形式）
-        schedule.enter(0, 0, self.execute_command, (cmd, inc))
+        now = datetime.datetime.now()
+        schedule_time = datetime.datetime.strptime(time_str,'%H:%M').replace(year=now.year,month=now.month,day=now.day)
+        if schedule_time < now:
+            schedule_time = schedule_time + datetime.timedelta(days=1)
+        time_before_start = int(round((schedule_time-datetime.datetime.now()).total_seconds()))
+        print u'mytimer => 还有%s秒开始任务' %time_before_start
+        schedule.enter(time_before_start, 0, self.execute_command, (cmd, inc))
         schedule.run()
 
 if __name__ == '__main__':
     mytimer = mytimer()
-    mytimer.cmd_timer("netstat -an", 60)
+    mytimer.cmd_timer("netstat -an", '15:07', 60)
