5067a002b60c6e7ed5a0d02b80a3177df4581e0b
start712 <start712@protonmail.com>
2017-10-13 10:43:01 +0800

    first add

---
+++ requests_manager.py
@@ -0,0 +1,87 @@
+# -*- coding:utf-8 -*-  
+"""
+--------------------------------
+    @Author: Dyson
+    @Contact: Weaver1990@163.com
+    @file: requests_manager.py
+    @time: 2017/10/13 9:49
+--------------------------------
+"""
+import sys
+import os
+import random
+import requests
+
+sys.path.append(sys.prefix + "\\Lib\\MyWheels")
+reload(sys)
+sys.setdefaultencoding('utf8')
+import set_log  # log_obj.debug(文本)  "\x1B[1;32;41m (文本)\x1B[0m"
+
+log_obj = set_log.Logger('requests_manager.log', set_log.logging.WARNING,
+                         set_log.logging.DEBUG)
+log_obj.cleanup('requests_manager.log', if_cleanup=True)  # 是否需要在每次运行程序前清空Log文件
+
+user_agent_list = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
+    "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "
+    "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "
+    "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "
+    "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "
+    "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "
+    "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "
+    "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+    "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "
+    "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "
+    "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+
+class requests_manager(object):
+    def __init__(self):
+
+        self.user_agent = random.choice(user_agent_list)
+        self.headers = {'Accept': '*/*',
+                        'Accept-Language': 'en-US,en;q=0.8',
+                        'Cache-Control': 'max-age=0',
+                        'User-Agent': self.user_agent,
+                        # 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+                        'Connection': 'keep-alive'
+                        }
+
+    def get_html(self, url):
+        self.headers['User-Agent'] = random.choice(user_agent_list)
+        print "user_agent:", self.headers['User-Agent']
+        resp = requests.get(url, headers=self.headers)
+        resp.encoding = resp.apparent_encoding
+        resp.raise_for_status()
+        html = resp.text
+        resp.close()
+        return html
+
+
+if __name__ == '__main__':
+    pass
